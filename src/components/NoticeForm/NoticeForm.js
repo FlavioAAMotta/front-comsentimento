b111e58f39141d2c483de91aeab46b6af7d0f123
FlavioAAMotta <flavio.motta@sga.pucminas.br>
2022-06-07 15:26:59 -0300

    Layout: Form de criar edital

--- src/components/NoticeForm/NoticeForm.js
+++ src/components/NoticeForm/NoticeForm.js
@@ -1,53 +1,48 @@
-import { FormContainer, FieldContainer, CheckField } from './styled-form'
+import { FormContainer, CancelContainer, SendContainer, CheckField, TitleContainer, DescriptionContainer, FileField, ButtonsContainer } from './styled-form'
 
 export const NoticeForm = (props) => {
     return (
-        <FormContainer onSubmit={props.onSubmit}>
-            <FieldContainer>
+        <FormContainer>
+            <TitleContainer
+                type="text"
+                name="title"
+                placeholder="Título"
+                value={props.form.title}
+                onChange={props.onChange}
+            />
+            <DescriptionContainer
+                name="description"
+                cols="40" rows="5"
+                value={props.form.description}
+                onChange={props.onChange} placeholder="Descrição"
+            />
+
+            <input
+                type="date"
+                name="date"
+                value={props.form.date}
+                onChange={props.onChange}
+            />
+            <CheckField>
+                <p>Ativo: </p>
                 <input
-                    type="text"
-                    name="title"
-                    placeholder="Título"
-                    value={props.form.title}
-                    onChange={props.onChange}
+                    type="checkbox"
+                    name="status"
+                    value={props.form.status}
+                    onChange={props.onChangeStatus}
                 />
-                <p onClick={props.onCancel}>X</p>
-            </FieldContainer>
-            <FieldContainer>
-                <textarea
-                    name="description"
-                    cols="40" rows="5"
-                    value={props.form.description}
-                    onChange={props.onChange} placeholder="Descrição"/>
-            </FieldContainer>
-            <FieldContainer>
-                <input
-                    type="date"
-                    name="date"
-                    value={props.form.date}
-                    onChange={props.onChange}
-                />
-                <CheckField><p>Ativo: </p>
-                    <input
-                        type="checkbox"
-                        name="status"
-                        value={props.form.status}
-                        onChange={props.onChangeStatus}
-                    />
-                </CheckField>
-            </FieldContainer>
-            <FieldContainer>
-                <input
-                    type="file"
-                    name="filePath"
-                    value={props.form.filePath}
-                    onChange={props.onChange}
-                    accept="application/pdf"
-                />
-            </FieldContainer>
-            <FieldContainer>
-                <button>ENVIAR</button>
-            </FieldContainer>
+            </CheckField>
+            <FileField
+                type="file"
+                name="filePath"
+                value={props.form.filePath}
+                onChange={props.onChange}
+                accept="application/pdf"
+            />
+            <ButtonsContainer>
+                <SendContainer onClick={props.onSubmit}>ENVIAR</SendContainer>
+                <CancelContainer onClick={props.onCancel}>CANCELAR</CancelContainer>
+            </ButtonsContainer>
         </FormContainer>
     )
 }
--- src/components/NoticeForm/styled-form.js
+++ src/components/NoticeForm/styled-form.js
@@ -1,25 +1,55 @@
 import styled from "styled-components"
+import { lightBackground } from '../../constants/colors';
 
 export const FormContainer = styled.form`
     margin: 1rem;
-    display: flex;
-    flex-direction: column;
-    background-color: white;
-    width: 80%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 2fr 1fr 1fr 1fr;
+    row-gap: 2px;
+    column-gap: 2px;
+    background-color: ${lightBackground};
+`
+export const TitleContainer = styled.input`
+    grid-column: 1/-1;
 `
 
-export const FieldContainer = styled.div`
+export const DescriptionContainer = styled.textarea`
+    grid-column: 1/-1;
+`
+
+export const ButtonsContainer = styled.div`
+    padding-top: 8px;
+    grid-column: 1/-1;
     display: flex;
     flex-direction: row;
-    width: 100%;
-    input {
-        width: 100%;
-    }
+    justify-content: space-evenly;
+    align-items: center;
+`
+
+export const SendContainer = styled.button`
+    background-color: #5c4cf7;
+    border: none;
+    color: white;
+    padding: 16px 0px;
+    width: 128px;
+    cursor: pointer;
+    `
+
+export const CancelContainer = styled.button`
+    background-color: #fa4949;
+    border: none;
+    color: white;
+    padding: 16px 0px;
+    width: 128px;
+    cursor: pointer;
 `
 
 export const CheckField = styled.div`
     display: flex;
     flex-direction: row;
-    width: 15%;
     align-items: center;
+`
+export const FileField = styled.input`
+    align-self: center;
 `
